fa9c5cd5d3bcff5427c5cd6f05bd78c66f44a4c2
Patrick Bell <patrickb97@hotmail.com>
2020-09-09 10:17:41 -0500

    #10 Created Basics of sending email functionality.

--- ec_email_client/src/App.js
+++ ec_email_client/src/App.js
@@ -15,7 +15,8 @@ function App() {
 
     // Authorization scopes required by the API; multiple scopes can be
     // included, separated by spaces.
-    var SCOPES = "https://www.googleapis.com/auth/gmail.readonly";
+    var SCOPES =
+        "https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/gmail.compose https://www.googleapis.com/auth/gmail.modify";
 
     function handleClientLoad() {
         console.log("About to loadwindow.gapi");
@@ -68,6 +69,19 @@ function App() {
         console.log("Sign in status: " + isSignedIn);
         if (isSignedIn) {
             console.log("Profile Info: ", await getProfileInfo("me"));
+            // let messageIds = await getMessagesIds();
+            // console.log(messageIds);
+            // console.log(await getMessage(messageIds[0].id));
+            // console.log(await getAllMessages());
+            let messageToSend = createMessage(
+                "ECCLIENT",
+                "test@email.com",
+                "TestEmail",
+                "ecclient@mailinator.com",
+                "EC Email",
+                "Hey, this is a test email from EC client...dope"
+            );
+            // sendMessage(messageToSend);
             authorizeButton.style.display = "none";
             signoutButton.style.display = "block";
         } else {
@@ -98,9 +112,9 @@ function App() {
         return decodeBase64(input);
     }
 
-    async function listMessagesIds(userId) {
+    async function getMessagesIds(userId) {
         // To get userId of logged in user, give "me"
-        if (userId == undefined) userId = "me";
+        if (userId === undefined) userId = "me";
 
         return new Promise((resolve, reject) => {
             window.gapi.client.gmail.users.messages
@@ -113,9 +127,18 @@ function App() {
         });
     }
 
-    async function listMessage(messageId) {
+    async function getMessage(messageId) {
         // https://developers.google.com/gmail/api/reference/rest/v1/users.messages.attachments#MessagePartBody
-        console.log("Getting specific message for " + messageId);
+        // console.log("Getting specific message for " + messageId);
+
+        let message = {
+            bodyHTML: null,
+            bodyText: null,
+            from: null,
+            to: null,
+            subject: null,
+            headers: {},
+        };
 
         return new Promise((resolve, reject) => {
             window.gapi.client.gmail.users.messages
@@ -124,19 +147,57 @@ function App() {
                     id: messageId,
                 })
                 .then(function (response) {
-                    console.log(response.result);
-                    resolve(
-                        decodeBase64HTML(
+                    // console.log(response.result);
+
+                    // If it has two payload parts
+                    if (
+                        !!response.result.payload.parts &&
+                        response.result.payload.parts.length > 1
+                    ) {
+                        message.bodyText = decodeBase64HTML(
+                            response.result.payload.parts[0].body.data
+                        );
+                        message.bodyHTML = decodeBase64HTML(
                             response.result.payload.parts[1].body.data
-                        )
-                    );
+                        );
+                    } else if (
+                        !!response.result.payload.body &&
+                        !!response.result.payload.body.data
+                    ) {
+                        message.bodyText = decodeBase64HTML(
+                            response.result.payload.body.data
+                        );
+                    } else {
+                        console.log(
+                            "Failed getting message body for:",
+                            response.result
+                        );
+                    }
+
+                    // Gets all headers, turns in to dict
+                    let headers = {};
+                    response.result.payload.headers.forEach((header) => {
+                        headers[header.name] = header.value;
+                        if (header.name === "Subject") {
+                            message.subject = header.value;
+                        }
+                        if (header.name === "To") {
+                            message.to = header.value;
+                        }
+                        if (header.name === "From") {
+                            message.from = header.value;
+                        }
+                    });
+                    message.headers = headers;
+
+                    resolve(message);
                 });
         });
     }
 
     async function getProfileInfo(userId) {
         // To get userId of logged in user, give "me"
-        if (userId == undefined) userId = "me";
+        if (userId === undefined) userId = "me";
 
         return new Promise((resolve, reject) => {
             window.gapi.client.gmail.users
@@ -149,37 +210,114 @@ function App() {
         });
     }
 
-    const [gapiIsLoaded, setGapiIsLoaded] = useState(false);
+    function createMessage(
+        fromName,
+        fromEmail,
+        toName,
+        toEmail,
+        subject,
+        messageContent
+    ) {
+        // https://developers.google.com/gmail/api/reference/rest/v1/users.messages#Message
+        // https://whatismyipaddress.com/email-header
+        // Look at this sample: https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/gmail/send.js
+        // Fix https://stackoverflow.com/questions/30590988/failed-sending-mail-through-google-api-with-javascript
+
+        const utf8Subject = `=?utf-8?B?${Buffer.from(subject).toString(
+            "base64"
+        )}?=`;
+        const messageParts = [
+            `From: ${fromName} <${fromEmail}>`,
+            `To: ${toName} <${toEmail}>`,
+            "Content-Type: text/html; charset=utf-8",
+            "MIME-Version: 1.0",
+            `Subject: ${utf8Subject}`,
+            "",
+            messageContent,
+        ];
+        const message = messageParts.join("\n");
+
+        // The body needs to be base64url encoded.
+        const encodedMessage = Buffer.from(message)
+            .toString("base64")
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_")
+            .replace(/=+$/, "");
+
+        return encodedMessage;
+    }
+
+    async function getAllMessages(userId) {
+        // To get userId of logged in user, give "me"
+        if (userId === undefined) userId = "me";
+        const numOfMessagesToGet = 10;
+
+        let messageIds = await getMessagesIds(userId);
+        let messages = [];
+        for (var i = 0; i < numOfMessagesToGet; i++) {
+            try {
+                messages.push(await getMessage(messageIds[i].id));
+            } catch {
+                console.log("Failed on ", messageIds[i].id);
+            }
+        }
+
+        return messages;
+    }
+
+    async function sendMessage(encodedMessage) {
+        // Look at this sample: https://github.com/googleapis/google-api-nodejs-client/blob/master/samples/gmail/send.js
+        // Fix https://stackoverflow.com/questions/30590988/failed-sending-mail-through-google-api-with-javascript
+
+        window.gapi.client.gmail.users.messages
+            .send({
+                userId: "me",
+                resource: {
+                    raw: encodedMessage,
+                },
+            })
+            .then((result) => {
+                console.log(result);
+            });
+    }
 
     // Process: handleClientLoad -> initClient -> updateSigninStatus
 
     useEffect(() => {
         const script = document.createElement("script");
-
         script.src = "https://apis.google.com/js/api.js";
         script.async = true;
-
         document.body.appendChild(script);
-        // setGapiIsLoaded(true);
+
+        // Handles Auth after load
+        setTimeout(() => {
+            handleClientLoad();
+        }, 1000);
+
         return () => {
             document.body.removeChild(script);
         };
     }, []);
 
-    // Just waits 2 seconds for it to load.
-    // TODO: CHANGE THIS, just for testing rn
-    useEffect(() => {
-        setTimeout(() => {
-            handleClientLoad();
-        }, 1000);
-    }, [gapiIsLoaded]);
-
     return (
         <div className="App">
             <div className="Title">EC Email Client</div>
             <div className="GoogleSigninButton">
-                <button className="GoogleSigninButton" id="authorize_button" style={{ display: "none", border: "none", color: "transparent", background: "transparent", textAlign: "center" }}>
-                    <img style={{ cursor: "pointer" }} src="https://raw.githubusercontent.com/react-native-community/google-signin/master/img/signin-button.png?sanitize=false" />
+                <button
+                    className="GoogleSigninButton"
+                    id="authorize_button"
+                    style={{
+                        display: "none",
+                        border: "none",
+                        color: "transparent",
+                        background: "transparent",
+                        textAlign: "center",
+                    }}
+                >
+                    <img
+                        style={{ cursor: "pointer" }}
+                        src="https://raw.githubusercontent.com/react-native-community/google-signin/master/img/signin-button.png?sanitize=false"
+                    />
                 </button>
             </div>
             <button id="signout_button" style={{ display: "none" }}>
