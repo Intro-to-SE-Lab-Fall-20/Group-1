9bab6ecc994441c6bc0314f20c497cb01dbd3d4b
David Mentgen III <Dmentgen3@gmail.com>
2020-11-05 20:02:16 -0600

    Removed unnecessary files; Updated travis CI auotmated test script

--- ec_email_client/src/tests/Endpoints.test.js
+++ ec_email_client/src/tests/Endpoints.test.js
@@ -19,86 +19,86 @@ test('[ENDPOINT] Server Health', function() {
   return expect(healthEndpointStatusCode).to.eventually.equal(200);
 })
 
-// test('[ENDPOINT] Server Signin', function() {
-//   var signinEndpointResult = fetch('http://3.133.110.55:8000/signIn', {
-//     method: 'post',
-//     headers: {'Content-Type':'application/json'},
-//     body: JSON.stringify({
-//       'username': 'test',
-//       'passwordHash': 'test'
-//     })
-//   }).then (function (result) {
-//     return result.status;
-//   })
+test('[ENDPOINT] Server Signin', function() {
+  var signinEndpointResult = fetch('http://3.133.110.55:8000/signIn', {
+    method: 'post',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      'username': 'test',
+      'passwordHash': 'test'
+    })
+  }).then (function (result) {
+    return result.status;
+  })
 
-//   return expect(signinEndpointResult).to.eventually.equal(200);
-// })
+  return expect(signinEndpointResult).to.eventually.equal(200);
+})
 
-// test('[ENDPOINT] Server GetNoteText', function() {
-//   var signinEndpointResult = fetch('http://3.133.110.55:8000/getNoteText', {
-//     method: 'post',
-//     headers: {'Content-Type':'application/json'},
-//     body: JSON.stringify({
-//       'username': 'test',
-//       'noteName': 'test',
-//       'token': 'test'
-//     })
-//   }).then (function (result) {
-//     return result.status;
-//   })
+test('[ENDPOINT] Server GetNoteText', function() {
+  var signinEndpointResult = fetch('http://3.133.110.55:8000/getNoteText', {
+    method: 'post',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      'username': 'test',
+      'noteName': 'test',
+      'token': 'test'
+    })
+  }).then (function (result) {
+    return result.status;
+  })
 
-//   return expect(signinEndpointResult).to.eventually.equal(401);
-// })
+  return expect(signinEndpointResult).to.eventually.equal(401);
+})
 
-// test('[ENDPOINT] Server GetNoteNames', function() {
-//   var signinEndpointResult = fetch('http://3.133.110.55:8000/getNoteText', {
-//     method: 'post',
-//     headers: {'Content-Type':'application/json'},
-//     body: JSON.stringify({
-//       'username': 'test',
-//       'noteName': 'test',
-//       'token': 'test'
-//     })
-//   }).then (function (result) {
-//     return result.status;
-//   })
+test('[ENDPOINT] Server GetNoteNames', function() {
+  var signinEndpointResult = fetch('http://3.133.110.55:8000/getNoteText', {
+    method: 'post',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      'username': 'test',
+      'noteName': 'test',
+      'token': 'test'
+    })
+  }).then (function (result) {
+    return result.status;
+  })
 
-//   return expect(signinEndpointResult).to.eventually.equal(401);
-// })
+  return expect(signinEndpointResult).to.eventually.equal(401);
+})
 
-// test('[ENDPOINT] Server UpdateNote', function() {
-//   var signinEndpointResult = fetch('http://3.133.110.55:8000/updateNote', {
-//     method: 'post',
-//     headers: {'Content-Type':'application/json'},
-//     body: JSON.stringify({
-//       'username': 'test',
-//       'noteName': 'test',
-//       'noteText': 'test',
-//       'token': 'test'
-//     })
-//   }).then (function (result) {
-//     return result.status;
-//   })
+test('[ENDPOINT] Server UpdateNote', function() {
+  var signinEndpointResult = fetch('http://3.133.110.55:8000/updateNote', {
+    method: 'post',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      'username': 'test',
+      'noteName': 'test',
+      'noteText': 'test',
+      'token': 'test'
+    })
+  }).then (function (result) {
+    return result.status;
+  })
 
-//   return expect(signinEndpointResult).to.eventually.equal(401);
-// })
+  return expect(signinEndpointResult).to.eventually.equal(401);
+})
 
-// test('[ENDPOINT] Server AddNote', function() {
-//   var signinEndpointResult = fetch('http://3.133.110.55:8000/addNote', {
-//     method: 'post',
-//     headers: {'Content-Type':'application/json'},
-//     body: JSON.stringify({
-//       'username': 'test',
-//       'noteName': 'test',
-//       'noteText': 'test',
-//       'token': 'test'
-//     })
-//   }).then (function (result) {
-//     return result.status;
-//   })
+test('[ENDPOINT] Server AddNote', function() {
+  var signinEndpointResult = fetch('http://3.133.110.55:8000/addNote', {
+    method: 'post',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      'username': 'test',
+      'noteName': 'test',
+      'noteText': 'test',
+      'token': 'test'
+    })
+  }).then (function (result) {
+    return result.status;
+  })
 
-//   return expect(signinEndpointResult).to.eventually.equal(401);
-// })
+  return expect(signinEndpointResult).to.eventually.equal(401);
+})
 
 // Disabled until completion of server
 // test('[ENDPOINT] Server AddUser', function() {
