f52943ac0bde057be9c414d04fbd610a4bc64d04
David Mentgen III <Dmentgen3@gmail.com>
2020-11-05 19:25:01 -0600

    Initial commit to see how travis handles api test

--- ec_email_client/src/App.test.js
+++ ec_email_client/src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
+import { assert } from 'chai';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/EC Email Client/i);
-  expect(linkElement).toBeInTheDocument();
-});
+var chai = require('chai');
+var expect = require('chai').expect;
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+test('derp', function() {
+  var healthEndpoint = fetch('http://3.133.110.55:8000/health').then(
+    function (result) {
+      return result.status;
+    }
+  )
+
+  return expect(healthEndpoint).to.eventually.equal(200);
+})
+
+
+// test('renders ec app selection page', () => {
+//   const { getByText } = render(<App />); 
+//   const Title = getByText(/EC Apps/i);
+//   const ECEmailClientButton = getByText(/EC > Gmail/i);
+//   const ECNotepadButton = getByText(/I love notes!/i);
+
+//   expect(Title).toBeInTheDocument();
+//   expect(ECEmailClientButton).toBeInTheDocument();
+//   expect(ECNotepadButton).toBeInTheDocument();
+// });
