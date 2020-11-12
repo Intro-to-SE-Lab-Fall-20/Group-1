28a525985516998b243b30b95146603ca7e152c0
David Mentgen III <Dmentgen3@gmail.com>
2020-11-11 18:06:40 -0600

    Finalized selenium gui testing and npm testing and travis ci testing

--- ec_email_client/src/tests/App.test.js
+++ ec_email_client/src/tests/App.test.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { queryByAttribute, queryByTestId, render } from '@testing-library/react';
 import App from '../App.js';
-import LoginPage from '../pages/LoginPage.js';
-import '@testing-library/jest-dom/extend-expect';
-//import { expect } from 'chai';
-import InboxPageComponent from '../components/InboxPageComponent.js';
+import { assert, expect } from "chai";
 
 test('[APP] Main Page Successfully Renders', () => {
   const { getByText } = render(<App />); 
-  const Title = getByText(/EC Apps/i);
-  const ECEmailClientButton = getByText(/EC > Gmail/i);
-  const ECNotepadButton = getByText(/I love notes!/i);
+  const Title = getByText(/EC Client/i);
+  const UsernameEntry = getByText(/Username:/i);
+  const PasswordEntry = getByText(/Password:/i);
 
-  expect(Title).toBeInTheDocument();
-  expect(ECEmailClientButton).toBeInTheDocument();
-  expect(ECNotepadButton).toBeInTheDocument();
+  assert.isNotNull(Title);
+  assert.isNotNull(UsernameEntry);
+  assert.isNotNull(PasswordEntry);
 });
